a5504c772b37ca68090ea4f1aefb4d21985cd83d
Bayathy <misosoup0321@icloud.com>
2024-05-09 16:01:17 +0900

    feat: 🚀 retejsの実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
+import { createEditor } from "@/components/editor/editor";
+import React from "react";
+import { useRete } from "rete-react-plugin";
 
 export default function Home() {
-  return <Button>test</Button>;
+  const [ref] = useRete(createEditor);
+
+  return <div className="h-screen w-screen" ref={ref}></div>;
 }

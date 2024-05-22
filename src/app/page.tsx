82fca2eca0d4de3204715dcaa0ff89efcd05c69f
Bayathy <misosoup0321@icloud.com>
2024-05-22 11:39:12 +0900

    chore: 🔨 ratejsの削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-"use client";
-
-import { createEditor } from "@/components/editor/editor";
 import React from "react";
-import { useRete } from "rete-react-plugin";
 
 export default function Home() {
-  const [ref] = useRete(createEditor);
-
   return <div className="h-screen w-screen" ref={ref}></div>;
 }
